c338246b0cb8d4bf7646bd041a03a57052463341
lvov <lvov.sergey.2001@gmail.com>
2025-08-06 16:45:03 +0300

    hotfix: change timeout

--- internal/render/render.go
+++ internal/render/render.go
@@ -31,7 +31,7 @@ func CliRender(layer models.Layer, z, x, y int) (image.Image, error) {
 					return nil, err
 				}
 			}
-		case <-time.After(120 * time.Second):
+		case <-time.After(10 * time.Minute):
 			return nil, errors.New("Истекло время ожидания воркера")
 		}
 		file, err = os.Open(filePath + fileName)
