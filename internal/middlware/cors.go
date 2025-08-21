a2abd91e74d1a53529529dca0a1a3799d054d520
lvov <lvov.sergey.2001@gmail.com>
2025-08-21 16:54:27 +0300

    remove comment

--- internal/middlware/cors.go
+++ internal/middlware/cors.go
@@ -9,7 +9,7 @@ func Init(mux http.Handler) http.Handler {
 
 func cors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Разрешаем все домены
+
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
